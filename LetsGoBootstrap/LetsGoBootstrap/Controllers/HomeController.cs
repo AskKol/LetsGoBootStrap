3563975b3c34fdd94557828641668656968d883f
Kolapo Abolarinwa <kolapo.abolarinwa@slimmingworld.co.uk>
2017-02-27 16:48:14 +0000

    Structuring layout page
    Added the needed nuget packages, bootstrap and started styling the layout page.

---
+++ LetsGoBootstrap/LetsGoBootstrap/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsGoBootstrap.Controllers
+{
+    public class HomeController:Controller
+    {
+        /// <summary>
+        /// The home/ landing page of the website
+        /// </summary>
+        /// <returns>A view</returns>
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
